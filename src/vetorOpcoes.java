1739155ebca054a1462f4c0b8e64d87825098a52
IanMouraF <ianmourafac@gmail.com>
2023-02-20 21:03:03 -0300

    1° commit

---
+++ src/vetorOpcoes.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class vetorOpcoes {
+    public void inserirDado(){
+        Scanner inteiro = new Scanner(System.in);
+        int inteiroVetor = inteiro.nextInt();
+
+    }
+}

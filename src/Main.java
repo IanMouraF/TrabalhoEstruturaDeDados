2e8e1b695a5593aae909f4edbda73c5a92f794ee
Kaiwawa <rafael_c_baumann@outlook.com.br>
2023-02-20 23:43:06 -0300

    oi

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void Menu() {
-
+    static int x = 0;
+    static boolean y = true;
+    public static int Menu() {
         System.out.println(
                 "------------------------------\n" +
                 "|   1 -  Vetor               |\n" +
                 "|   2 -  Lista               |\n" +
                 "------------------------------\n" +
                 "Selecione a opcao desejada: ");
-
+                Scanner scan = new Scanner(System.in);
+                x = scan.nextInt();
+                return x;
     }
 
-    public static void VerificarNumero(x){
-        if (x == 1){
-            vo.vetorOpcoes();
-        }if (x == 2){
-            
-        }else{
-            
+    public static void loop(){
+        vetorOpcoes vo = new vetorOpcoes();
+        listaOpcoes lo = new listaOpcoes();
+
+        while (y == true){
+            Menu();
+    
+            if (x == 1 || x == 2){
+                if (x == 1) {
+                    vo.vetorOpcoes();
+                }if (x == 2) {
+                    lo.listaOpcoes();
+                }
+            y = false;
+            }else{
+                System.out.println("escreveu errado burro");
+            }
         }
-        return x;
     }
 
     public static void main(String[] args) {
-        
-        vetorOpcoes vo = new vetorOpcoes();
-        
-        Menu();
-        Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
-        
-        while (x != 1 | x != 2){
-        VerificarNumero(x);
-        }
+
+    loop();    
+       
     }
 }

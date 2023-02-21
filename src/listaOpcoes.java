427ae56326b56956b18e6deb957d4b323660fb22
Samuel-MA7 <s4muelm0ura@gmail.com>
2023-02-21 18:31:52 -0300

    No
    Criação de classe No e método de inserir em listaOpcoes.

--- src/listaOpcoes.java
+++ src/listaOpcoes.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class listaOpcoes {
     
-    public void listaOpcoes(){
-        
+    Scanner scan = new Scanner(System.in);
+
+    public void inserir(){
+        System.out.println("Escolha um número inteiro para adicionar");
+        int n = scan.nextInt();
+        No lista = new No(n);
+        System.out.println(lista);
+    }
+
+    public static void main(String[] args) {
+        listaOpcoes l = new listaOpcoes();
+        l.inserir();
     }
 
 }

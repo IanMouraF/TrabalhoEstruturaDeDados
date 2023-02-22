26bac83fd4246d109bc474d99999d5980ebbf684
Samuel-MA7 <s4muelm0ura@gmail.com>
2023-02-22 18:13:31 -0300

    inserir
    método ínserir

--- src/listaOpcoes.java
+++ src/listaOpcoes.java
@@ -2,18 +2,38 @@ import java.util.Scanner;
 
 public class listaOpcoes {
     
+    No inicio, fim;
+    int tamanho;
+
     Scanner scan = new Scanner(System.in);
 
     public void inserir(){
-        System.out.println("Escolha um número inteiro para adicionar");
+        No lista = new No();
+        System.out.println("Número:");
         int n = scan.nextInt();
-        No lista = new No(n);
-        System.out.println(lista);
+        lista.dado = n;
+        lista.proximo = null;
+        lista.anterior = fim;
+        if(fim != null){
+            fim.proximo = lista;
+        }
+        fim = lista;
+        if(tamanho == 0){
+            inicio = fim;
+        }
+        tamanho++;
+        System.out.println(tamanho);
     }
 
+
     public static void main(String[] args) {
+        
         listaOpcoes l = new listaOpcoes();
-        l.inserir();
+        for(int i=0; i<5; i++){
+            l.inserir();
+        }
+           
     }
+
 
 }

16e850ac01ee7aa08ff2c41245c04d5c81705b10
Samuel-MA7 <s4muelm0ura@gmail.com>
2023-02-22 18:41:19 -0300

    inserir e exibir
    métodos inserir e exibir

--- src/listaOpcoes.java
+++ src/listaOpcoes.java
@@ -2,38 +2,49 @@ import java.util.Scanner;
 
 public class listaOpcoes {
     
-    No inicio, fim;
+    private static class No {
+        int dado;
+        No proximo;
+        No anterior;
+    }
+
+    No inicio;
+    No fim;
     int tamanho;
-
+    
     Scanner scan = new Scanner(System.in);
-
-    public void inserir(){
-        No lista = new No();
-        System.out.println("Número:");
-        int n = scan.nextInt();
-        lista.dado = n;
-        lista.proximo = null;
-        lista.anterior = fim;
-        if(fim != null){
-            fim.proximo = lista;
+    
+    public void inserir() {
+        No novoNo = new No();
+        System.out.println("Digite o valor a ser inserido:");
+        novoNo.dado = scan.nextInt();
+        novoNo.proximo = null;
+        novoNo.anterior = fim;
+        if (fim != null) {
+            fim.proximo = novoNo;
         }
-        fim = lista;
-        if(tamanho == 0){
+        fim = novoNo;
+        if (tamanho == 0) {
             inicio = fim;
         }
         tamanho++;
-        System.out.println(tamanho);
     }
 
-
-    public static void main(String[] args) {
-        
-        listaOpcoes l = new listaOpcoes();
-        for(int i=0; i<5; i++){
-            l.inserir();
+    public void exibir() {
+        No atual = inicio;
+        while (atual != null) {
+            System.out.print(atual.dado + " ");
+            atual = atual.proximo;
         }
-           
+        System.out.println();
     }
-
-
+    
+    public static void main(String[] args) {
+        listaOpcoes lista = new listaOpcoes();
+        for (int i = 0; i < 5; i++) {
+            lista.inserir();
+        }
+        lista.exibir();
+    }
+    
 }

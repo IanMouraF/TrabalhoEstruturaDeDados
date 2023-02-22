749698d8aba7e72e8c4c2df40e217c0cedd892ff
Samuel-MA7 <s4muelm0ura@gmail.com>
2023-02-22 18:13:31 -0300

    inserir
    método ínserir

--- src/No.java
+++ src/No.java
@@ -2,16 +2,10 @@ public class No {
     
     int dado;
     No anterior, proximo;
-
-    public No(int novoDado){
-        dado = novoDado;
-        anterior = null;
-        proximo = null;
-    }
-
+    
     @Override
     public String toString() {
-        return "No [anterior=" + anterior + ", dado=" + dado + " , proximo=" + proximo + "]";
+        return "No [anterior=" + anterior + ", dado=" + dado + ", proximo=" + proximo + "]";
     }
 
 }
